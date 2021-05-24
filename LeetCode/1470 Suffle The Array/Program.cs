ae6887a59610e54a7f3296fb0e78754305783059
Martin Peralez <martin.peralez@outlook.com>
2021-05-24 11:40:00 -0500

    1470 Successful Submission 1672 Work in Progress

---
+++ LeetCode/1470 Suffle The Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1470_Suffle_The_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution test = new Solution();
+            int[] nums = new int[] { 2, 5, 1, 3, 4, 7 };
+
+            test.Shuffle(nums, 3);
+        }
+    }
+    public class Solution
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            List<int> list = new List<int>();
+            int j = n;
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(nums[i]);
+                list.Add(nums[j]);
+                j++;
+            }
+            int[] array = list.ToArray();
+            return array;
+        }
+    }
+}
+
+//Success
+//Details 
+//Runtime: 240 ms, faster than 71.17% of C# online submissions for Shuffle the Array.
+//Memory Usage: 33 MB, less than 72.64% of C# online submissions for Shuffle the Array.
---
+++ LeetCode/1672 Richest Customer Wealth/Program.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+
+//namespace _1672_Richest_Customer_Wealth
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[][] accounts = new int[][] {{1, 2, 3 }, {3, 2, 1 } };
+//            Solution test = new Solution();
+//            test.MaximumWealth(accounts);
+//        }
+//    }
+//    public class Solution
+//    {
+//        public int MaximumWealth(int[][] accounts)
+//        {
+//            int wealth = 0;
+//            for (int i = 0; i < accounts.Length; i++)
+//            {
+
+//            }
+//            return 0;
+//        }
+//    }
+//}
